e0226eb34b197305329f7ca54fae502d57a3fefe
akihiro <aponmaru@gmail.com>
2022-12-25 14:25:51 +0900

    [add] videojs で welcome に表示するように実装

--- resources/scripts/main.ts
+++ resources/scripts/main.ts
@@ -1,12 +1,15 @@
 import { createApp, h } from 'vue'
 import { createInertiaApp } from '@inertiajs/inertia-vue3'
 import { resolvePageComponent } from 'vite-plugin-laravel/inertia'
+import VueVideoPlayer from '@videojs-player/vue'
+import 'video.js/dist/video-js.css'
 
 createInertiaApp({
 	resolve: (name) => resolvePageComponent(name, import.meta.glob('../views/pages/**/*.vue')),
 	setup({ el, app, props, plugin }) {
 		createApp({ render: () => h(app, props) })
 		.use(plugin)
+		.use(VueVideoPlayer)
 		.mount(el)
 	},
 })
